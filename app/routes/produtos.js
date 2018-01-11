473c9ee5a7cdaea1789db7564d91c92e49f797be
Daniel <daniel@spacelab.com.br>
2018-01-11 16:48:02 -0200

    adição do banco de dados

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,28 @@
 // configura rota para página de produtos
 module.exports = (app) => {
+
   app.get('/produtos', (req, res) => {
-    res.render("produtos/lista");
+
+    const SQL = require('mysql');
+
+    // cria conexão com o banco
+    const connection = SQL.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'daniel',
+      database: 'casadocodigo'
+    });
+
+    // @param query feita no banco
+    // @param função dispara quando a consulta for concluída
+    connection.query('select * from produtos', (err, results) => {
+      // variavel lista usará os valores retornados 
+      res.render('produtos/lista', {lista: results});
+    });
+
+    // fecha conexão
+    connection.end();
+
   });
+
 }
